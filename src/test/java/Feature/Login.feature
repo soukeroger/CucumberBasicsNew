854fd51f07b37901343fb456026887c23302a468
Roger Souke <Roger.Souke@centre4.local>
2018-01-17 11:18:23 +0000

    New scenario and method

--- src/test/java/Feature/Login.feature
+++ src/test/java/Feature/Login.feature
@@ -4,7 +4,20 @@ Feature: LoginFeature
   Scenario: Login with correct username and password
     Given I navigate to the login page
     And I enter the following for login
-      | UserName | Password      |
+      | username | password      |
       | admin    | adminpassword |
     And I click the login button
     Then I should see the userform page
+
+
+  Scenario Outline: Login with correct username and password using Scenario outline
+    Given I navigate to the login page
+    And I enter <username> and <password>
+    And I click the login button
+    Then I should see the userform page
+
+    Examples:
+      | username | password   |
+      | execute  | automation |
+      | admin    | admin      |
+      | testing  | qa         |
--- src/test/java/Steps/LoginSteps.java
+++ src/test/java/Steps/LoginSteps.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,9 +39,35 @@ public class LoginSteps {
 
     @And("^I enter the following for login$")
     public void iEnterTheFollowingForLogin(DataTable table) throws Throwable {
-       List<List<String>> data = table.raw();
+       /* List<List<String>> data = table.raw();
        System.out.println("The Value is:" + data.get(0).get(0).toString());
-        System.out.println("The Value is:" + data.get(0).get(1).toString());
+        System.out.println("The Value is:" + data.get(0).get(1).toString());*/
+        List<User> users = new ArrayList<User>();
+//     Store all the users
+        users = table.asList(User.class);
 
+        for (User user : users) {
+            System.out.println("The UserName is" + user.username);
+            System.out.println("The Password is" + user.password);
+        }
+
+    }
+
+    @And("^I enter ([^\"]*) and ([^\"]*)$")
+    public void iEnterUsernameAndPassword(String userName, String password) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+        System.out.println("UserName is : " + userName);
+        System.out.println("Password is : " + password);
+    }
+
+    public class User {
+        public String username;
+        public String password;
+
+        public User(String userName, String passWord) {
+            username = userName;
+            password = passWord;
+        }
     }
 }
