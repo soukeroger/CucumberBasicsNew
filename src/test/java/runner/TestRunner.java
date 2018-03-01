75738272a20485ed58599bd58389883a84ae2928
soukeroger <souke.roger@yahoo.co.uk>
2018-03-01 21:37:54 +0000

    Running test with testng

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,7 +1,16 @@
 package runner;
 
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 /**
  * Created by Souke Family on 28/02/2018.
  */
-public class TestRunner {
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/feature"}, glue = "Steps")
+
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
